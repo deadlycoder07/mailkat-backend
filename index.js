d95e4f1e43462b5f5eafc04055557c83df0213cf
Abha Jha <jhaabha08@gmail.com>
2021-06-27 00:00:46 +0530

    access origin changed

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.use(flash());
 
 app.use((req, res, next) => {
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
   
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
